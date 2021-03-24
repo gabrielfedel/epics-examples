1de2efbf79f95739e150d597bdb4b3a3fb1ba842
Gabriel Fedel <gabriel.fedel@esss.se>
2021-03-24 14:45:41 +0100

    Include autosave example

---
+++ autosave/st.cmd
@@ -0,0 +1,10 @@
+require autosave
+
+epicsEnvSet("AS_TOP", "$(E3_CMD_TOP)")
+epicsEnvSet("IOCNAME", "TESTIOC")
+epicsEnvSet("SETTINGS_FILES","settings")
+iocshLoad("$(autosave_DIR)/autosave.iocsh")
+
+#afterInit("fdbrestore("$(AS_TOP)/$(IOCNAME)/save/$(SETTINGS_FILES).sav")")
+
+dbLoadDatabase("ai.db")
