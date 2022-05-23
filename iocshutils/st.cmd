26e6705fd85fdf8df36bd29aa11ba77e0537beb0
Gabriel Fedel <gabriel.fedel@esss.se>
2022-05-23 15:04:47 +0200

    Include a tests with bigwaveforms with 14Hz update

---
+++ iocshutils/st.cmd
@@ -0,0 +1,4 @@
+require iocshutils
+addScan 0.071
+
+dbLoadRecords("db14.db")
---
+++ iocshutils/st_bigwave.cmd
@@ -0,0 +1,26 @@
+require iocshutils
+addScan 0.071
+
+require calc
+
+dbLoadRecords("db14.db")
+dbLoadRecords("fastbigwaveforms.db", "CH=1")
+dbLoadRecords("fastbigwaveforms.db", "CH=2")
+dbLoadRecords("fastbigwaveforms.db", "CH=3")
+dbLoadRecords("fastbigwaveforms.db", "CH=4")
+dbLoadRecords("fastbigwaveforms.db", "CH=5")
+dbLoadRecords("fastbigwaveforms.db", "CH=6")
+dbLoadRecords("fastbigwaveforms.db", "CH=7")
+dbLoadRecords("fastbigwaveforms.db", "CH=8")
+dbLoadRecords("fastbigwaveforms.db", "CH=9")
+dbLoadRecords("fastbigwaveforms.db", "CH=10")
+dbLoadRecords("fastbigwaveforms.db", "CH=11")
+dbLoadRecords("fastbigwaveforms.db", "CH=12")
+dbLoadRecords("fastbigwaveforms.db", "CH=13")
+dbLoadRecords("fastbigwaveforms.db", "CH=14")
+dbLoadRecords("fastbigwaveforms.db", "CH=15")
+dbLoadRecords("fastbigwaveforms.db", "CH=16")
+dbLoadRecords("fastbigwaveforms.db", "CH=17")
+dbLoadRecords("fastbigwaveforms.db", "CH=18")
+dbLoadRecords("fastbigwaveforms.db", "CH=19")
+dbLoadRecords("fastbigwaveforms.db", "CH=20")
